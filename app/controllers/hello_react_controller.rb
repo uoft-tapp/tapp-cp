66ed4f243ad63d6b934a549ce268b856d4a7012d
simpslandyy <simpslandyy@gmail.com>
2017-07-14 09:34:46 -0400

    changed db name, fixed spelling/spacing errors

--- app/controllers/hello_react_controller.rb
+++ app/controllers/hello_react_controller.rb
@@ -81,13 +81,13 @@ class Contract
       font_size 12
       text "Dear #{@first_name} #{@last_name},"
       move_down 5
-      text "We are please to offer you a position as a Teaching Assistant in the Department of Computer Science " +
-      "during the period of <b>#{@start_date}</b> to <b>#{@end_date}</b>. Your assignment will be <b>#{@position}</b>. (We reserver the " +
+      text "We are pleased to offer you a position as a Teaching Assistant in the Department of Computer Science " +
+      "during the period of <b>#{@start_date}</b> to <b>#{@end_date}</b>. Your assignment will be <b>#{@position}</b>. (We reserve the " +
       "right to change your appointment from this course to another course, if necessary). The total time involved " +
-      "in the assignment is <b>#{@hours}</b>. The salary for this position is governed by the collective aggreement " +
+      "in the assignment is <b>#{@hours}</b>. The salary for this position is governed by the collective agreement " +
       "between the University of Toronto and the Canadian Union of Public Employees, Local 3902, a copy of which " +
       "will be provided upon request. The current rate of pay for UG = $43.65; SGS I and SGS II/PDF = $43.65. " +
-      "According to the collective aggreement the total sum paid to you for the appintment described above will be :",
+      "According to the collective agreement the total sum paid to you for the appointment described above will be :",
       indent_paragraphs: 30, inline_format: true
     end
   end
@@ -106,7 +106,7 @@ class Contract
   def insert_cupe
     grid([6,0],[8,0]).bounding_box do
       text "If this is your first appointment  after  becoming  a  Ph.D.  student,  you  will  be  guaranteed  three  more " +
-      "appointments of the same number of hours in subsequent years as per Article 16.05 of the CUPE 3902 (Unit1) Collective Agreement." +
+      "appointments of the same number of hours in subsequent years as per Article 16.05 of the CUPE 3902 (Unit 1) Collective Agreement." +
       "If this is not your first appointment, you may still be owed one or more subsequent " +
       "appointments under the aforementioned Article. If this is the case, you may consult the letter you received last April, where your " +
       "appointment status was summarized. Please feel free to contact Karen Reid to seehow various guarantees fit together.",
@@ -114,16 +114,16 @@ class Contract
 
       move_down 10
 
-      text "You will soon be given the opportunity to review the Description of Duties and Allocation of Hours(DDAH) form, which will set out " +
+      text "You will soon be given the opportunity to review the Description of Duties and Allocation of Hours (DDAH) form, which will set out " +
       "more specifically the duties of your position, and the hours assigned to each.",
       indent_paragraphs: 30
 
-      text "Please also be advised that in accepting this	offer	of appointment, you are accepting the responsibility " +
-      "during	the	course of	your appointment for contacting	your supervisor immediately if you have any questions " +
+      text "Please also be advised that in accepting this offer of appointment, you are accepting the responsibility " +
+      "during the course of your appointment for contacting	your supervisor immediately if you have any questions " +
       "or concerns relating to the hours of	work as set out in your job description, in order	that these may be " +
       "discussed and resolved at the earliest opportunity. Do not make any alterations or add any written " +
       "statements to the form, or the offer of employment will be invalidated.",
-      indent_paragraphs: 30
+    indent_paragraphs: 30
     end
 
   end
