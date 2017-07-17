491811cc0b96119a883e346c39cff6143add34db
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-17 15:07:30 -0400

    rescue from connection error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from (Errno::ECONNREFUSED) do |e|
+    render status: :not_found, json: { status: 404 }
+  end
 end
