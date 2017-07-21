dc1bead55dcf95777fdba1dacbda23521d75cc1a
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-21 00:29:47 -0400

    make run_cp.sh update .env file

--- run_cp.sh
+++ run_cp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 temp="`docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' tapp_rails-app_1`"
-echo "TAPP=$temp" > .env
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'file=($(cat .env))'
+file[6]="TAPP=$temp"
+printf "%s\n" "${file[@]}" > .env
 docker-compose up
