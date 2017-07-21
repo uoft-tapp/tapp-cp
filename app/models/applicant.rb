94a7806c8f363dfa4a500a81feae84d90c1cdb61
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-21 11:27:32 -0400

    create offer_importer

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,4 +2,13 @@ require 'active_resource'
 require 'net/http'
 class Applicant < ActiveResource::Base
   self.site = "http://#{ENV['TAPP']}:3000/"
+
+  def self.find_by(utorid)
+    Applicant.all.as_json.each do |applicant|
+      if applicant["utorid"]==utorid
+        return applicant
+      end
+    end
+    return nil
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-offer = Offer.create!(
-  position_id: 1,
-  applicant_id: 1,
-  hours: 60,
-)
-contract = offer.create_contract!(
-  position_id: 1,
-  applicant_id: 1,
-  link: "my secret",
-  deadline: Time.now + (2*7*24*60*60)
-)
-puts "db insert done"
+importer = OfferImporter.new("mock_offers")
+importer.import_data
