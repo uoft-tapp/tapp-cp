8b032243cdbb5649a237ef8672a36e7956b38c82
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-24 11:38:50 -0400

    remove run_cp.sh

--- run_cp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp dev.env.default .env
-temp="`docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' tapp_rails-app_1`"
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'file=($(cat .env))'
-file[6]="TAPP=$temp"
-printf "%s\n" "${file[@]}" > .env
-docker-compose up
