bbaefd1cf52672fa04da81a4d90ad24dd65b0090
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-26 16:37:46 -0400

    set sender as ta-hr-admin@cs.toronto.edu

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -4,6 +4,7 @@ class AppController < ActionController::Base
 
   def test
     @contracts = Contract.all.map { |c| c.format }
+    @offers = Offer.all.map {|o| o.format }
     render :test, layout: false
   end
 
