42bdcc90f73df984761458a66dc42576fef1775e
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-08-29 13:39:44 -0400

    changed session dates to input fields (non-functional for now)

--- app/javascript/cp/components/sessionsForm.js
+++ app/javascript/cp/components/sessionsForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel, Tabs, Tab, Form, InputGroup, FormGroup, FormControl } from 'react-bootstrap';
+import { Panel, Tabs, Tab, InputGroup, FormControl } from 'react-bootstrap';
 
 class SessionsForm extends React.Component {
     render() {
@@ -9,39 +9,61 @@ class SessionsForm extends React.Component {
                     <Tab title={<b>Sessions</b>} disabled />
                     {this.props.appState.getSessionsList().map((session, sessionId) =>
                         <Tab title={session.get('semester') + ' ' + session.get('year')}>
-		        <Form
-			    inline
-			    onSubmit={event => {
-                                event.preventDefault();
-                                if (event.target.elements[0].value != session.get('pay')) {
-				    this.props.appState.updateSessionPay(
-					sessionId, event.target.elements[0].value);
-				}
-                            }}>
-				<b>Start date:</b>&ensp;
-				{new Date(session.get('startDate')).toDateString()}&emsp;&emsp;
-				<b>End date:</b>&ensp;
-				{new Date(session.get('endDate')).toDateString()}&emsp;&emsp;
+                            <div className="form-inline">
+                                <b>Start date:</b>&ensp;
+                                <form
+                                    className="form-group"
+                                    onSubmit={event => {
+                                        event.preventDefault();
+                                    }}>
+                                    <FormControl
+                                        type="date"
+                                        defaultValue={session.get('startDate').split('T', 1)[0]}
+                                    />
+                                </form>&emsp;&emsp;
+                                <b>End date:</b>&ensp;
+                                <form
+                                    className="form-group"
+                                    onSubmit={event => {
+                                        event.preventDefault();
+                                    }}>
+                                    <FormControl
+                                        type="date"
+                                        defaultValue={session.get('endDate').split('T', 1)[0]}
+                                    />
+                                </form>&emsp;&emsp;
                                 <b>Pay:</b>&ensp;
-                                <FormGroup>
+                                <form
+                                    className="form-group"
+                                    onSubmit={event => {
+                                        event.preventDefault();
+                                        if (event.target.elements[0].value != session.get('pay')) {
+                                            this.props.appState.updateSessionPay(
+                                                sessionId,
+                                                event.target.elements[0].value
+                                            );
+                                        }
+                                    }}>
                                     <InputGroup>
                                         <InputGroup.Addon>$</InputGroup.Addon>
                                         <FormControl
-					    type="number"
-					    min="0.00"
-				            step="0.01"
+                                            type="number"
+                                            name="pay"
+                                            min="0.00"
+                                            step="0.01"
                                             defaultValue={session.get('pay')}
-				            onBlur={event => {
-						if (event.target.value != session.get('pay')) {
-						    this.props.appState.updateSessionPay(
-							sessionId,
-							event.target.value);
-						}
-					    }}
-				        />
+                                            onBlur={event => {
+                                                if (event.target.value != session.get('pay')) {
+                                                    this.props.appState.updateSessionPay(
+                                                        sessionId,
+                                                        event.target.value
+                                                    );
+                                                }
+                                            }}
+                                        />
                                     </InputGroup>
-                                </FormGroup>
-                            </Form>
+                                </form>
+                            </div>
                         </Tab>
                     )}
                 </Tabs>
