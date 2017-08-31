5d169f4fd56793a8c85b039344fb640022956d6e
Mathew Zaleski <matz@zaleski.ca>
2017-08-31 16:53:57 -0400

    make the prod and dev shell scripts are similar as possible.
    TODO: refactor or perhaps rm the dev version.

--- dev-run-clean.sh
+++ dev-run-clean.sh
@@ -5,7 +5,8 @@ die(){
 	exit 2
 }
 
-# start up tapp for development 
+# start up tapp for development
+# TODO: either toss the dev version or factor it with the production version
 
 echo 'have you shut down containers already with docker down?'
 echo 'examine running containers, if any in table below'
@@ -16,13 +17,13 @@ echo 'if there are any tapp containers listed above you should consider '
 echo 'docker-compose down, perhaps even down -v (to remove the postgres volume)'
 echo
 
-read -p 'hit enter to cp dev.env.devfault .env : ' JUNK
+read -p 'hit enter to continue: [interrupt to quit]: '
+
+read -p 'enter to cp dev.env.devfault .env :' JUNK
 
 (set -x
 cp dev.env.default .env || die failed to copy dev.env.default
 )
-
-#TODO: factor following. currently code copy of prod version!
 
 read -p 'enter to `docker rm -f` your containers: ' JUNK
 
@@ -30,16 +31,22 @@ read -p 'enter to `docker rm -f` your containers: ' JUNK
 docker-compose rm -f  || die docker-compose rm -f failed
 )
 
-read -p 'enter to `docker-compose up tapp containers: ' JUNK
+read -p 'enter to `docker-compose build --force-rm`  ' JUNK
+
+(set -x
+docker-compose build --force-rm || die docker-compose up --force-recreate failed
+)
+
+read -p 'enter to `docker-compose up tapp containers: (will want -d in production) ' JUNK
 
 (set -x
 docker-compose up -d --force-recreate || die docker-compose up --force-recreate failed
 )
 
+#there is probably a race condition here. migrate can't work until container is really up.
 read -p 'enter to `migrate postgres db: ' JUNK
 
 (set -x
 docker-compose run rails-app rake db:migrate  || die "rake db:migrate failed"
 )
-
 
