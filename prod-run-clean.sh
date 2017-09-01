baf04513a5046190faacefe3910281c22e155c04
TAPP runner <tapp-runner@tapp.cs.toronto.edu>
2017-09-01 18:15:22 -0400

    [HOTFIX] run migrate in production container
    more like temporary hack, really. migrate wasn't taking. in admin container. fixme.

--- prod-run-clean.sh
+++ prod-run-clean.sh
@@ -15,7 +15,7 @@ echo
 echo 'if there are any tapp containers listed above you should consider '
 echo 'docker-compose down, perhaps even down -v (to remove the postgres volume)'
 echo
-echo '*** to really nuke the images consider `docker system prune --all --force` ***'
+echo '*** to really nuke the containers consider `docker system prune --all --force` ***'
 echo
 
 read -p 'hit enter to continue: [interrupt to quit]: '
@@ -23,7 +23,7 @@ read -p 'hit enter to continue: [interrupt to quit]: '
 read -p 'enter to cp prod.env.devfault .env :' JUNK
 
 (set -x
-cp prod.env.default .env || die failed to copy prod.env.default
+cp prod.env.default .env
 )
 
 read -p 'enter to `docker rm -f` your containers: ' JUNK
@@ -35,13 +35,13 @@ docker-compose rm -f  || die docker-compose rm -f failed
 read -p 'enter to `docker-compose build --force-rm`  ' JUNK
 
 (set -x
-docker-compose build --force-rm || die docker-compose up --force-recreate failed
+docker-compose build --force-rm || die docker-compose build  --force-rm failed
 )
 
-read -p 'enter to `docker-compose up -d --force-recreate` tapp containers: ' JUNK
+read -p 'enter to `docker-compose up tapp containers: (will want -d in production) ' JUNK
 
 (set -x
-docker-compose up -d --force-recreate || die docker-compose up --force-recreate failed
+docker-compose up -d  || die docker-compose up failed
 )
 
 #there is probably a race condition here. migrate can't work until container is really up.
@@ -51,11 +51,17 @@ docker-compose up -d --force-recreate || die docker-compose up --force-recreate 
 
 read -p 'enter to `migrate postgres db: ' JUNK
 
+# (set -x
+#   docker-compose \
+#    -f docker-compose.yml \
+#    -f docker-compose.production.yml \
+#    -f docker-compose.production-admin.yml \
+#    run rails-app rake db:migrate 
+# )|| die "rake db:migrate failed"
+
+
 (set -x
   docker-compose \
-   -f docker-compose.yml \
-   -f docker-compose.production.yml \
-   -f docker-compose.production-admin.yml \
    run rails-app rake db:migrate 
 )|| die "rake db:migrate failed"
 
