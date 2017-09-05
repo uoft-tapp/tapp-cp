8ad49622a446bf2690208cb4c813addb3f453ac0
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-09-05 18:25:01 -0400

    add ! to model insert in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or create!d alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Department.create(name: "Computer Science")
+#   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create!(name: 'Luke', movie: movies.first)
+Department.create!(name: "Computer Science")
 
-Duty.create(name: "Training")
-Duty.create(name: "Additional Training (if required)")
-Duty.create(name: "Preparation")
-Duty.create(name: "Contact Time")
-Duty.create(name: "Marking/Grading")
-Duty.create(name: "Other Duties")
+Duty.create!(name: "Training")
+Duty.create!(name: "Additional Training (if required)")
+Duty.create!(name: "Preparation")
+Duty.create!(name: "Contact Time")
+Duty.create!(name: "Marking/Grading")
+Duty.create!(name: "Other Duties")
 
-Training.create(name: "Attending Health and Safety training session")
-Training.create(name: "Meeting with supervisor")
-Training.create(name: "Adapting Teaching Techniques (ATT) (scaling learning activities)")
+Training.create!(name: "Attending Health and Safety training session")
+Training.create!(name: "Meeting with supervisor")
+Training.create!(name: "Adapting Teaching Techniques (ATT) (scaling learning activities)")
 
-Category.create(name: "Discussion-based Tutorial")
-Category.create(name: "Skill Development Tutorial")
-Category.create(name: "Review and Q&A Session")
-Category.create(name: "Laboratory/Practical")
+Category.create!(name: "Discussion-based Tutorial")
+Category.create!(name: "Skill Development Tutorial")
+Category.create!(name: "Review and Q&A Session")
+Category.create!(name: "Laboratory/Practical")
