32af6aba0c8ee7c9d36ad33e6d1da31ccbc8163d
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-09-06 11:16:11 -0400

    replaced logout fetch function with a logout form

--- app/javascript/tapp/components/navbar.js
+++ app/javascript/tapp/components/navbar.js
@@ -191,7 +191,22 @@ const Auth = props => {
                     </MenuItem>
             )}
 
-            <MenuItem onClick={() => props.logout()}>Logout</MenuItem>
+            <MenuItem
+                onClick={() => {
+                    var form = document.createElement('form');
+                    form.action = '/logout';
+                    form.method = 'post';
+
+                    var currPage = document.createElement('input');
+                    currPage.name = 'current_page';
+                    currPage.value = document.location.pathname;
+
+                    form.appendChild(currPage);
+                    document.body.append(form);
+                    form.submit();
+                }}>
+                Logout
+            </MenuItem>
         </NavDropdown>
     );
 };
