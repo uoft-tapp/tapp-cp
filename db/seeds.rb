d61d6b9ce53c739d17a5e4d7a14206c492e57641
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-10-06 16:34:16 -0400

    [hotfix] fix seed.rb errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,36 @@
 #
 #   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create!(name: 'Luke', movie: movies.first)
-Duty.create!(name: "Training")
-Duty.create!(name: "Additional Training (if required)")
-Duty.create!(name: "Preparation")
-Duty.create!(name: "Contact Time")
-Duty.create!(name: "Marking/Grading")
-Duty.create!(name: "Other Duties")
-
-Training.create!(name: "Attending Health and Safety training session")
-Training.create!(name: "Meeting with supervisor")
-Training.create!(name: "Adapting Teaching Techniques (ATT) (scaling learning activities)")
-
-Category.create!(name: "Discussion-based Tutorial")
-Category.create!(name: "Skill Development Tutorial")
-Category.create!(name: "Review and Q&A Session")
-Category.create!(name: "Laboratory/Practical")
+duty = [
+  "Training",
+  "Additional Training (if required)",
+  "Preparation",
+  "Contact Time",
+  "Marking/Grading",
+  "Other Duties",
+]
+training = [
+  "Attending Health and Safety training session",
+  "Meeting with supervisor",
+  "Adapting Teaching Techniques (ATT) (scaling learning activities)",
+]
+category = [
+  "Discussion-based Tutorial",
+  "Skill Development Tutorial",
+  "Review and Q&A Session",
+  "Laboratory/Practical",
+]
+def insert_seed_data(model, items, type)
+  items.each do |item|
+    data = model.find_by(name: item)
+    if !data
+      model.create!(name: item)
+      puts "#{type} with name '#{item}' inserted."
+    else
+      puts "#{type} with name '#{item}' already exists."
+    end
+  end
+end
+insert_seed_data(Duty, duty, "Duty")
+insert_seed_data(Training, training, "Training")
+insert_seed_data(Category, category, "Category")
