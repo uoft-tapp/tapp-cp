83464991f2f3ebd9794c0b71adf10bc2daa0ff58
Rathu Kugan <rathu.kugan@gmail.com>
2018-03-21 23:18:40 -0400

    Added instructor-pref migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923043351) do
+ActiveRecord::Schema.define(version: 20180322031441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170923043351) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "round_id"
+    t.integer "instructor_pref"
     t.index ["applicant_id"], name: "index_applications_on_applicant_id"
   end
 
