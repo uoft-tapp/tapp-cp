3d42caba749e92e9f424731c8a04f2d9310a7306
michellemtchai <michelle.chai@mail.utoronto.ca>
2018-04-30 14:55:57 -0400

    finish rspec for exports

--- spec/controllers/export_controller_spec.rb
+++ spec/controllers/export_controller_spec.rb
@@ -29,6 +29,51 @@ RSpec.describe ExportController, type: :controller do
     )
   end
 
+  let(:no_offer_position) do
+    Position.create!(
+    position: "CSC411 - test 2",
+    round_id: "110",
+    open: true,
+    campus_code: 1,
+    course_name: "Introduction to Software Testing",
+    current_enrolment: 50,
+    duties: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget dignissim sem. Curabitur at semper eros. Aenean nec sem lobortis, scelerisque mi at, aliquam diam. Mauris malesuada elit nibh, sed hendrerit nulla mattis sed. Mauris laoreet imperdiet dictum. Pellentesque risus nulla, varius ut massa ut, venenatis fringilla sapien. Cras eget euismod augue, eget dignissim erat. Cras nec nibh ullamcorper ante rutrum dapibus sed nec tellus. In hac habitasse platea dictumst. Suspendisse semper tellus ac sem tincidunt auctor.",
+    qualifications: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget dignissim sem. Curabitur at semper eros. Aenean nec sem lobortis, scelerisque mi at, aliquam diam. Mauris malesuada elit nibh, sed hendrerit nulla mattis sed. Mauris laoreet imperdiet dictum. Pellentesque risus nulla, varius ut massa ut, venenatis fringilla sapien. Cras eget euismod augue, eget dignissim erat. Cras nec nibh ullamcorper ante rutrum dapibus sed nec tellus. In hac habitasse platea dictumst. Suspendisse semper tellus ac sem tincidunt auctor.",
+    hours: 22,
+    estimated_count: 15,
+    estimated_total_hours: 330,
+    session_id: session.id,
+    cap_enrolment: nil,
+    num_waitlisted: nil,
+    start_date: "2017-09-01 00:00:00 UTC",
+    end_date: "2017-12-31 00:00:00 UTC",
+    )
+  end
+
+  let(:applicant_1) do
+    Applicant.create!(
+    utorid: "cookie226",
+    app_id: "22",
+    student_number: 1234567890,
+    first_name: "Landy",
+    last_name: "Simpson",
+    dept: "Computer Science",
+    program_id: "4UG",
+    yip: 4,
+    email: "simps@mail.com",
+    phone: "4165558888",
+    address: "100 Jameson Ave Toronto, ON M65-48H")
+  end
+
+  let(:offer) do
+    Offer.create!(
+    position_id: position[:id],
+    applicant_id: applicant_1[:id],
+    hours: 60,
+    link: "mangled-link")
+  end
+
+
   context "when there are no applicants in the system" do
     context "when calling #transcript_access" do
       it "returns status 404 and an error message" do
@@ -176,6 +221,31 @@ RSpec.describe ExportController, type: :controller do
   end
 
   describe "GET /export/ddahs/:position_id" do
-
+    context "when expected" do
+      it "returns a CSV file" do
+        offer.reload
+        get :ddahs, params: {position_id: position[:id]}
+        expect(response.status).to eq(200)
+        expect(response.content_type).to eq("text/csv")
+        expect(response.header["Content-Disposition"]).to eq(
+          "attachment; filename=\"#{position[:position]}_ddahs.csv\"")
+      end
+    end
+    context "when no offers for position" do
+      it "returns a status 404 error" do
+        get :ddahs, params: {position_id: no_offer_position[:id]}
+        message = {message: "Warning: There are currenly no offers for this position. Operation aborted"}
+        expect(response.status).to eq(404)
+        expect(response.body).to eq(message.to_json)
+      end
+    end
+    context "when position does not exist" do
+      it "returns a status 404 error" do
+        get :ddahs, params: {position_id: 'poops'}
+        message = {message: "Warning: There is no such position"}
+        expect(response.status).to eq(404)
+        expect(response.body).to eq(message.to_json)
+      end
+    end
   end
 end
